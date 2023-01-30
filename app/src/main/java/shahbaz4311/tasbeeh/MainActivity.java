ceda4999bb93ba09947d6a715df0fe8783cdce6f
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2023-01-30 21:36:40 +0500

    completed input recycle view

--- app/src/main/java/shahbaz4311/tasbeeh/MainActivity.java
+++ app/src/main/java/shahbaz4311/tasbeeh/MainActivity.java
@@ -1,19 +1,45 @@
 package shahbaz4311.tasbeeh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import shahbaz4311.tasbeeh.utils.RecordAdapter;
+
+public class MainActivity extends AppCompatActivity {
     RecyclerView inputView;
+    List<String> tasbeehNames;
+    RecyclerView.Adapter recordAdapter;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing
+        tasbeehNames=new ArrayList<>();
+
+        //getting tasbeeh names from db
+        tasbeehNames.add("abc");
+        tasbeehNames.add("def");
+        tasbeehNames.add("ghi");
+        tasbeehNames.add("abcdefghijklmnopqrstuvwxyz");
+
         inputView=findViewById(R.id.inputView);
+        layoutManager=new LinearLayoutManager(MainActivity.this);
+        inputView.setHasFixedSize(true);
+        inputView.setLayoutManager(layoutManager);
+
+
+        recordAdapter=new RecordAdapter(tasbeehNames);
+
+        //setting inputRecycleView adapter
+        inputView.setAdapter(recordAdapter);
 
 
     }
--- app/src/main/java/shahbaz4311/tasbeeh/utils/RecordAdapter.java
+++ app/src/main/java/shahbaz4311/tasbeeh/utils/RecordAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,39 +17,49 @@ import shahbaz4311.tasbeeh.R;
 
 public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.RecordVH> {
 
-    List<Record> recordList;
-    public RecordAdapter(List<Record> recordList){
-        this.recordList=recordList;
+    List<String> tasbeehNames;
+
+    public RecordAdapter(List<String> tasbeehNames) {
+        this.tasbeehNames = tasbeehNames;
     }
+
     @NonNull
     @Override
     public RecordVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View recordView= LayoutInflater.from(parent.getContext()).inflate(R.layout.recordinput,parent,false);
+        View recordView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recordinput, parent, false);
         return new RecordVH(recordView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecordVH holder, int position) {
-
-
+        holder.tasbeehName = tasbeehNames.get(position);
+        holder.nameInput.setText(holder.tasbeehName);
+        holder.countInput.setText("");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tasbeehNames.size();
     }
 
-    public class RecordVH extends RecyclerView.ViewHolder{
+    public class RecordVH extends RecyclerView.ViewHolder {
         TextView nameInput;
         CheckBox reciteInput;
         EditText countInput;
 
+        String tasbeehName;
 
         public RecordVH(@NonNull View itemView) {
             super(itemView);
-            nameInput=itemView.findViewById(R.id.nameInput);
-            reciteInput=itemView.findViewById(R.id.reciteInput);
-            countInput=itemView.findViewById(R.id.countInput);
+            nameInput = itemView.findViewById(R.id.nameInput);
+            reciteInput = itemView.findViewById(R.id.reciteInput);
+            countInput = itemView.findViewById(R.id.countInput);
+
+            //setting checked listener on reciteInput to enable/disable counter
+            reciteInput.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                if(isChecked) countInput.setEnabled(true);
+                else countInput.setEnabled(false);
+            });
 
 
         }
